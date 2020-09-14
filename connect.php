9ecc2c71678a0c31feab2752f05ec4d684925890
Palderjan <55434301+erkitammeaid@users.noreply.github.com>
2020-09-14 10:15:02 +0300

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,5 +1,10 @@
 <?php
 
+$host = 'd83309.mysql.zonevs.eu';
+$db   = 'd83309_books';
+$user = 'd83309sa327405';
+$charset = 'utf8mb4';
+
 require_once '../config.php';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
