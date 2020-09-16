6bea337818a185606c6a627422522fa36d5e26d2
Palderjan <55434301+erkitammeaid@users.noreply.github.com>
2020-09-16 13:51:04 +0300

    Update index.php

--- ajax/index.php
+++ ajax/index.php
@@ -4,7 +4,7 @@ $host = 'd83309.mysql.zonevs.eu';
 $db   = 'd83309_books';
 $user = 'd83309sa327405';
 $charset = 'utf8mb4';
-$pass = 'ametikool10';
+$pass = '';
 
 $stmt = $pdo->prepare('SELECT * FROM books');
 $stmt->execute();
